178aeb90d9ea725c8c6a9235ab2bc0fe62191003
16rean <16rean@gmail.com>
2021-04-21 12:38:31 +0900

    Signed-off-by: 16rean <16rean@gmail.com>

--- detect_anju_freeze.py
+++ detect_anju_freeze.py
@@ -178,9 +178,9 @@ def detect(file_name):
 
     if save_txt or save_img:
         #s = f"\n{len(list(save_dir.glob('labels/*.txt')))} labels saved to {save_dir / 'labels'}" if save_txt else ''
-        print(f"Results saved to {save_dir}{s}")
+        #print(f"Results saved to {save_dir}{s}")
 
-    print(f'Done. ({time.time() - t0:.3f}s)')
+    print(f'Done. ({time.time() - t0:.3f})')
 
     return labels
 
