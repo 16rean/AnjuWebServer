df8d51fa4b87c6e38798a47afdf119b685a185c7
16rean <16rean@gmail.com>
2021-04-21 12:30:09 +0900

    Signed-off-by: 16rean <16rean@gmail.com>

--- detect_anju_freeze.py
+++ detect_anju_freeze.py
@@ -148,22 +148,22 @@ def detect(file_name):
 
                 # Write results
                 print('-->Write results')
-                for *xyxy, conf, cls in reversed(det):
-                    if save_txt:  # Write to file
-                        #20210420 ysc  save label name
-                        #xywh = (xyxy2xywh(torch.tensor(xyxy).view(1, 4)) / gn).view(-1).tolist()  # normalized xywh
-                        #line = (cls, *xywh, conf) if opt.save_conf else (cls, *xywh)  # label format
-                        label = f'{names[int(cls)]} {conf:.2f}' # detected label name
-                        with open(txt_path + '.txt', 'a') as f:
-                            #f.write(('%g ' * len(line)).rstrip() % line +  '\n') # print labe map
-                            f.write(label +  '\n')
-
-                    if save_img or view_img:  # Add bbox to image
-                        label = f'{names[int(cls)]}-{conf:.2f}'
-                        plot_one_box(xyxy, im0, label=label, color=colors[int(cls)], line_thickness=3)
-
-                        print('-->label :' ,label)
-                    labels.append(label)
+                #for *xyxy, conf, cls in reversed(det):
+                #    if save_txt:  # Write to file
+                #        #20210420 ysc  save label name
+                #        #xywh = (xyxy2xywh(torch.tensor(xyxy).view(1, 4)) / gn).view(-1).tolist()  # normalized xywh
+                #        #line = (cls, *xywh, conf) if opt.save_conf else (cls, *xywh)  # label format
+                #        label = f'{names[int(cls)]} {conf:.2f}' # detected label name
+                #        with open(txt_path + '.txt', 'a') as f:
+                #            #f.write(('%g ' * len(line)).rstrip() % line +  '\n') # print labe map
+                #            f.write(label +  '\n')
+                #
+                #    if save_img or view_img:  # Add bbox to image
+                #        label = f'{names[int(cls)]}-{conf:.2f}'
+                #        plot_one_box(xyxy, im0, label=label, color=colors[int(cls)], line_thickness=3)
+                #
+                #        print('-->label :' ,label)
+                #    labels.append(label)
 
             # Print time (inference + NMS)
             print(f'{s}Done. ({t2 - t1:.3f}s)')
