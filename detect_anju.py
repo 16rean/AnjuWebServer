c54ed0fd668366f53630e964ae507ba2047a7090
16rean <76719646+16rean@users.noreply.github.com>
2021-04-24 07:20:47 +0900

    Update detect_anju.py

--- detect_anju.py
+++ detect_anju.py
@@ -178,45 +178,4 @@ def detect(file_name):
         print(f"Results saved to {save_dir}{s}")
 
     print(f'Done. ({time.time() - t0:.3f}s)')
-
     return labels
-
-
-
-
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--weights', nargs='+', type=str, default='best.pt', help='model.pt path(s)')
-    parser.add_argument('--source', type=str, default='uploads', help='source')  # file/folder, 0 for webcam
-    parser.add_argument('--img-size', type=int, default=640, help='inference size (pixels)')
-    parser.add_argument('--conf-thres', type=float, default=0.25, help='object confidence threshold')
-    parser.add_argument('--iou-thres', type=float, default=0.45, help='IOU threshold for NMS')
-    parser.add_argument('--device', default='', help='cuda device, i.e. 0 or 0,1,2,3 or cpu')
-    parser.add_argument('--view-img', action='store_true', help='display results')
-    parser.add_argument('--save-txt', default='-txt', help='save results to *.txt')
-    parser.add_argument('--save-conf', action='store_true', help='save confidences in --save-txt labels')
-    parser.add_argument('--nosave', action='store_true', help='do not save images/videos')
-    parser.add_argument('--classes', nargs='+', type=int, help='filter by class: --class 0, or --class 0 2 3')
-    parser.add_argument('--agnostic-nms', action='store_true', help='class-agnostic NMS')
-    parser.add_argument('--augment', action='store_true', help='augmented inference')
-    parser.add_argument('--update', action='store_true', help='update all models')
-    parser.add_argument('--project', default='static', help='save results to project/name')
-    parser.add_argument('--name', default='result_img', help='save results to project/name')
-    parser.add_argument('--exist-ok', default=True, help='existing project/name ok, do not increment')
-    opt = parser.parse_args()
-    print(opt)
-    #check_requirements(exclude=('pycocotools', 'thop'))
-    file_name = 'mask01.jpg'
-    with torch.no_grad():
-        if opt.update:  # update all models (to fix SourceChangeWarning)
-            for weights in ['yolov5s.pt', 'yolov5m.pt', 'yolov5l.pt', 'yolov5x.pt']:
-                detect(file_name)
-                strip_optimizer(weights)
-        else:
-            detect(file_name)
-
-
-
-
-
-
