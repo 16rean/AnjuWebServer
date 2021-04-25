fc78fe25ea8fe434078269997ed0d690ca0ad0d8
16rean <76719646+16rean@users.noreply.github.com>
2021-04-25 13:18:08 +0900

    Update detect_anju.py

--- detect_anju.py
+++ detect_anju.py
@@ -177,5 +177,7 @@ def detect(file_name):
         s = f"\n{len(list(save_dir.glob('labels/*.txt')))} labels saved to {save_dir / 'labels'}" if save_txt else ''
         print(f"Results saved to {save_dir}{s}")
 
-    print(f'Done. ({time.time() - t0:.3f}s)')
+    #print(f'Done. ({time.time() - t0:.3f}s)')
+    
+    
     return labels
